eb20d8648461e26acdfca4745dfac43ce4b8bbf7
Davide <Davide@10.239.40.1>
2017-12-10 15:56:15 +0000

    test

--- src/com/holonomix/commoninterface/ActiveAlarmInterface.java
+++ src/com/holonomix/commoninterface/ActiveAlarmInterface.java
@@ -7,6 +7,5 @@ import com.holonomix.list.ActiveList;
 public interface ActiveAlarmInterface {
 
 	public void connect(ActiveList<Alarm> activeList) throws AdapterException; 
-	public void disconnect();
-	
+	public void disconnect();	
 }
